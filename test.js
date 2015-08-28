1ac05da6025cc078e7bbc01582f20dfdeb2a9e3c
Doğan Derya <co3moz@gmail.com>
2015-08-28 11:39:32 +0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,55 @@
+// table part
+var garageTable = ezTable({
+  headers: {
+    car:   {
+      title:   'Car Information',
+      onPrint: function (o) {
+        return [o.car.brand, o.car.model, o.car.year].join(" ");
+      }
+    },
+    owner: {
+      title:   'Owner Information',
+      onPrint: function (o) {
+        return o.owner.name;
+      }
+    },
+    date:  {
+      title: 'Garage In'
+    }
+  }
+});
+
+
+var garage = [
+  {
+    id:    1,
+    car:   {
+      brand: "Toyota",
+      model: "Corolla",
+      year:  2014,
+    },
+    owner: {
+      id:   1,
+      name: "Doğan DERYA"
+    },
+    date:  new Date("2015-08-27 10:04")
+  },
+
+  {
+    id:    2,
+    car:   {
+      brand: "Toyota",
+      model: "Verso",
+      year:  2015,
+    },
+    owner: {
+      id:   2,
+      name: "Sadettin YAVUZ"
+    },
+    date:  new Date("2015-08-28 14:34")
+  }
+];
+
+// generate part
+garageTable.bind("garageTable"); // bind the table
+garageTable.load(garage); // load objects (clears old ones if exists)
