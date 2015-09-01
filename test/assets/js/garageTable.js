3ce5972f52d5790a4387c08f80ac927b8596dd2e
eray <relfishere@gmail.com>
2015-09-01 12:14:00 +0300

    Little Fix.

--- test/assets/js/garageTable.js
+++ test/assets/js/garageTable.js
@@ -30,12 +30,12 @@ var localData = [
 
 var garageTable = ezTable({
   headers: {
-    car:   {
-      title:   'Car Information',
+    car: {
+      title: 'Car Information',
       onPrint: function (o) {
         return [o.car.brand, o.car.model, o.car.year].join(" ");
       },
-      style:   {
+      style: {
         backgroundColor: "red"
       },
 
@@ -44,12 +44,12 @@ var garageTable = ezTable({
       }
     },
     owner: {
-      title:   'Owner Information',
+      title: 'Owner Information',
       onPrint: function (o) {
         return o.owner.name;
       }
     },
-    date:  {
+    date: {
       title: 'Garage In',
 
       onPrint: function (o) {
@@ -67,39 +67,38 @@ var garageTable = ezTable({
   },
 
   buttons: [{
-    title:   'delete',
-    icon:    'assets/img/delete.png',
+    title: 'delete',
+    icon: 'assets/img/delete.png',
     onClick: function (o, e) {
       garageTable.remove(o);
     }
   }, {
-    title:   'go',
-    icon:    'assets/img/go.png',
+    title: 'go',
+    icon: 'assets/img/go.png',
     onClick: function (o) {
       alert(o.owner.name);
     }
   },
-    {title: 'Hey!', icon:    'assets/img/go.png'}]
+    {title: 'Hey!', icon: 'assets/img/go.png'}]
 });
 
 // generate part
 garageTable.bind("garageTable"); // bind the table
 /*
-yes, u can!
+  yes, u can!
 
-garageTable.load({
-  url:    "assets/data.json",
-  method: "GET",
-  swear:  false,
+  garageTable.load({
+    url:    "assets/data.json",
+    method: "GET",
+    swear:  false,
 
-  onLoad: function () {
-    console.log("data.json loaded");
-    garageTable.render();
-  }
-});
+    onLoad: function () {
+      console.log("data.json loaded");
+      garageTable.render();
+    }
+ });
 */
 garageTable.load(localData);
 garageTable.render();
 
 window.x = garageTable;
-
